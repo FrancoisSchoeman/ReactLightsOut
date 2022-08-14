6413740dd84963a6f1cae23fe7b00f27705dc8d1
Francois Schoeman <francois.schoeman15@gmail.com>
2022-08-14 15:34:24 +0200

    Updates

---
+++ precache-manifest.fd2d4b3ab9b9c6b574dc27ff3afbceda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d648ec1eabadc0841ea",
+    "url": "/static/css/main.cf1223a8.chunk.css"
+  },
+  {
+    "revision": "0d648ec1eabadc0841ea",
+    "url": "/static/js/main.0d648ec1.chunk.js"
+  },
+  {
+    "revision": "6657e2f43ced2ab3b0c9",
+    "url": "/static/js/1.6657e2f4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1d4fd208ec831cb76169dca014014690",
+    "url": "/index.html"
+  }
+];
